0607f81219d0e2781ca6ba7a10160cc208ce1199
linga harish kumar <lingaharishkumar@gmail.com>
2022-03-19 22:35:13 +0530

    initial

---
+++ script.js
@@ -0,0 +1,15 @@
+var password = document.getElementById("password");
+
+function genPassword(){
+
+   var chars = "abcdefghijklmnopqrstuvwxyz1234567890()!@#$%^&*_+-={}[]|:;<,>.?";
+   var plength = 10
+   var result = "";
+
+   for(var i=0;i<=plength;i++){
+      var rn = Math.floor(Math.random()*chars.length)
+      result += chars.substring(rn,rn+1)
+   }
+
+   document.getElementById("password").value = result;
+}
